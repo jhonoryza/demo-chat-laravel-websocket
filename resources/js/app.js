d04ac253e0d66d918a1844db22dabbdedb72562e
fajar sidik priatna <jardik.oryza@gmail.com>
2023-06-19 08:31:00 +0700

    add laravel breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import './bootstrap';
 
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
 const channel = Echo.channel('public.1');
 channel.subscribe(() => {
     console.log('subscribed');
