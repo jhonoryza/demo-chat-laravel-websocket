e5faea32b99123a135728003f5eb2a71ed6e006d
fajar sidik priatna <jardik.oryza@gmail.com>
2023-06-19 08:31:34 +0700

    add chat migration and model

---
+++ app/Models/Message.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'group_id',
+        'user_id',
+        'message'
+    ];
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
